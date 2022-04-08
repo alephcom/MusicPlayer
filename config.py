aabc5c58c9c1415c4a0ca99e335c7a5946a19e29
rada <rada.leng95@gmail.com>
2022-04-08 08:23:59 +0000

    first commit for issue #3

--- config.py
+++ config.py
@@ -47,6 +47,7 @@ class Config:
         self.ADMINS_ONLY: bool = os.environ.get("ADMINS_ONLY", False)
         self.SPOTIFY_CLIENT_ID: str = os.environ.get("SPOTIFY_CLIENT_ID", None)
         self.SPOTIFY_CLIENT_SECRET: str = os.environ.get("SPOTIFY_CLIENT_SECRET", None)
+        self.ENABLE_LOG: bool = os.getenv("ENABLE_LOG", 'False').lower() in ('true', '1', 't')
 
 
 config = Config()

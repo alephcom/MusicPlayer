92c7044ffb01ec4745e3d61520e31cae4501972a
rada <rada.leng95@gmail.com>
2022-04-22 10:26:55 +0000

    commit for issue #12

--- api_server.py
+++ api_server.py
@@ -5,6 +5,7 @@ from core import app
 from config import config
 from pytgcalls import CustomApi
 from pytgcalls import idle
+from core.groups import all_groups, get_group
 
 apiserver = CustomApi(config.API_SERVER_PORT)
 
@@ -13,12 +14,26 @@ async def handler(request: dict):
     """
         json request example:
             {
-                "chat_id" : -1022459474630,
-                "message" : "!stream https://us-tx-dallas-1.listentochurch.com:8443/9235230.mp3"
+                "type"          : "command", ("command" or "streams")
+                "chat_id"       : -1022459474630,
+                "message"       : "!stream https://us-tx-dallas-1.listentochurch.com:8443/9235230.mp3"
             }
     """
     try:
-        await app.send_message(int(request['chat_id']), request['message'])
+        if request['type'] == "command":
+            await app.send_message(int(request['chat_id']), request['message'])
+        elif request['type'] == "streams":
+            streams = []
+            for group_id in all_groups():
+                group = get_group(group_id)
+                if group['is_playing']:
+                    stream = group['now_playing'].to_dict()
+                    stream['chat_id'] = group_id
+                    streams.append(stream)
+            
+            return {'success' : True, 'streams' : streams}
+        else:
+            return {'success' : False, 'error' : 'unknown request type'}
     except:
         return {'success' : False}
 
